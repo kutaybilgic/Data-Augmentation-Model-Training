28cee39ef926dde438744867e93a3e5da123c4d2
Kutay Bilgiç <kutaybilgic_fb@hotmail.com>
2023-06-05 22:52:35 +0300

    fix

--- augmentation_v2.py
+++ augmentation_v2.py
@@ -2,50 +2,49 @@ import os
 import torch
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
+from PIL import Image
+import numpy as np
 from customDataset import ProductDataset
-from sklearn.model_selection import StratifiedKFold
 
-device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
 my_transforms = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize((256,256)),
-    transforms.RandomCrop((224,224)),
+    transforms.Resize((256, 256)),
+    transforms.RandomCrop((224, 224)),
     transforms.ColorJitter(brightness=0.5),
     transforms.RandomRotation(degrees=45),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomHorizontalFlip(p=0.05),
     transforms.RandomGrayscale(p=0.2),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.0, 0.0,0.0], std=[1.0,1.0,1.0])
-    ])
+    transforms.Normalize(mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0])
+])
 
-dataset = ProductDataset(csv_file='products.csv', root_dir='/Users/drivers/Desktop/ceng318', transform=my_transforms)
+root_dir = 'D:/Github/Data-Augmentation-Model-Training/products'
+product_folders = os.listdir(root_dir)
 
-n_splits = 5  # Change this value to the desired number of folds
-skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+# Tüm ürün klasörleri için augmentation yap
+for product_folder in product_folders:
+    label_dir = f'D:/Github/Data-Augmentation-Model-Training/train/{product_folder}'
+    os.makedirs(label_dir, exist_ok=True)
 
-# Get labels for stratification
-_, labels = zip(*dataset)
+    # Ürün klasörü içindeki her bir fotoğrafı augmente et
+    product_path = os.path.join(root_dir, product_folder)
+    image_files = os.listdir(product_path)
+    for image_file in image_files:
+        image_path = os.path.join(product_path, image_file)
 
-# Perform the K-Fold split
-img_num = 0
-for fold, (train_idx, val_idx) in enumerate(skf.split(torch.zeros(len(labels)), labels)):
-    os.makedirs(f'/Users/drivers/Desktop/ceng318/fold_{fold}', exist_ok=True)
+        # HEIC dosyası ise JPEG formatına dönüştür
+        if os.path.splitext(image_file)[1].lower() == '.heic':
+            image = Image.open(image_path)
+            image = image.convert('RGB')
+        else:
+            image = Image.open(image_path)
 
-    for idx in train_idx:
-        img, label = dataset[idx]
-        label_dir = f'/Users/drivers/Desktop/ceng318/fold_{fold}/train/label_{label}'
-        os.makedirs(label_dir, exist_ok=True)
-        save_image(img, f'{label_dir}/img{img_num}.png')
-        img_num += 1
+        # Her bir fotoğrafı 10 farklı şekilde augmente et
+        for i in range(10):
+            augmented_img = my_transforms(image)
+            save_image(augmented_img, f'{label_dir}/{image_file}_copy{i}.png')
 
-    for idx in val_idx:
-        img, label = dataset[idx]
-        label_dir = f'/Users/drivers/Desktop/ceng318/fold_{fold}/val/label_{label}'
-        os.makedirs(label_dir, exist_ok=True)
-        save_image(img, f'{label_dir}/img{img_num}.png')
-        img_num += 1
-
-print(img_num)
+print("Veriler başarıyla kaydedildi.")
